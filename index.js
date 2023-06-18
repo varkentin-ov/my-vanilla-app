9e88f9d6d66975f039a2f754d812d8833156bb01
Olena <varkentin.elena@gmail.com>
2023-06-18 17:40:05 +0300

    Move the site from Sandbox

---
+++ index.js
@@ -0,0 +1,59 @@
+let now = new Date();
+let date = document.querySelector("#date-form");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let newdate = `${day} ${hours}:${minutes}`;
+date.innerHTML = newdate;
+
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let formInput = document.querySelector("#search-input");
+  formInput = formInput.value;
+  let apiKey = "311f1f45fee82242ab4086372ab360f5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${formInput}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+let form = document.querySelector("#weather-search");
+form.addEventListener("submit", searchCity);
+
+function getPosition(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiKey = "311f1f45fee82242ab4086372ab360f5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+navigator.geolocation.getCurrentPosition(getPosition);
