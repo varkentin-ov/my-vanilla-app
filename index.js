4095cdc7ad30c77c78c183b45a9bbbe4103403fd
Olena <varkentin.elena@gmail.com>
2023-06-24 17:07:17 +0300

    Format date

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+let now = new Date();
+let date = document.querySelector("#date-form");
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
 }
-
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[day];
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
 }
-
-document.querySelector("#date-form").innerHTML = formatDate(
-  response.data.dt * 1000
-);
+let newdate = `${day} ${hours}:${minutes}`;
+date.innerHTML = newdate;
 
 function showWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
